544dc6ee74579642172d11ff8352b59764b7db44
Tomislav Busic <tomislav.busic89@gmail.com>
2022-08-11 18:45:31 +0200

    badge

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,11 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
+import FavoriteContext from '../../store/favorites-context';
 
 const MainNavigation = () => {
+    const favoritesCtx = useContext(FavoriteContext);
     return (
         <div className={classes.header}>
             <h1 className={classes.logo}>Meetups</h1>
@@ -15,7 +18,9 @@ const MainNavigation = () => {
                         <Link to='new-meetup'>New meetup</Link>
                     </li>
                     <li>
-                        <Link to='/favorites'>Favorites</Link>
+                        <Link to='/favorites'>Favorites
+                        <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
